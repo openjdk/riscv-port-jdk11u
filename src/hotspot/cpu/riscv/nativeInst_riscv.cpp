3fa279b459fffd1bd1ce158a7fdaa9d8704450a8
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2023-04-13 18:29:27 +0800

    Revert JDK-8212681: Refactor IC locking to use a fine grained CompiledICLocker

--- src/hotspot/cpu/riscv/nativeInst_riscv.cpp
+++ src/hotspot/cpu/riscv/nativeInst_riscv.cpp
@@ -146,8 +146,7 @@ address NativeCall::destination() const {
 // during code generation, where no patching lock is needed.
 void NativeCall::set_destination_mt_safe(address dest, bool assert_lock) {
   assert(!assert_lock ||
-         (Patching_lock->is_locked() || SafepointSynchronize::is_at_safepoint()) ||
-         CompiledICLocker::is_safe(addr_at(0)),
+         (Patching_lock->is_locked() || SafepointSynchronize::is_at_safepoint()),
          "concurrent code patching");
 
   ResourceMark rm;
