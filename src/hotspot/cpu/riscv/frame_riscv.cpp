7dfb40501029eeb024ab46162e608e862b8e6207
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2023-04-29 16:14:55 +0800

    Revert: JDK-8254231: Implementation of Foreign Linker API (Incubator) JDK-8264774: Implementation of Foreign Function and Memory API (Incubator)

--- src/hotspot/cpu/riscv/frame_riscv.cpp
+++ src/hotspot/cpu/riscv/frame_riscv.cpp
@@ -361,21 +361,6 @@ frame frame::sender_for_entry_frame(RegisterMap* map) const {
   return fr;
 }
 
-OptimizedEntryBlob::FrameData* OptimizedEntryBlob::frame_data_for_frame(const frame& frame) const {
-  ShouldNotCallThis();
-  return nullptr;
-}
-
-bool frame::optimized_entry_frame_is_first() const {
-  ShouldNotCallThis();
-  return false;
-}
-
-frame frame::sender_for_optimized_entry_frame(RegisterMap* map) const {
-  ShouldNotCallThis();
-  return {};
-}
-
 //------------------------------------------------------------------------------
 // frame::verify_deopt_original_pc
 //
