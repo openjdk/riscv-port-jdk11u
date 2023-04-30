9b0e81f58ebadf1b94a1a60d11b7cbec358441d9
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2023-04-30 16:50:51 +0800

    Revert JDK-8258459: Decouple gc_globals.hpp from globals.hpp

--- src/hotspot/cpu/riscv/templateTable_riscv.cpp
+++ src/hotspot/cpu/riscv/templateTable_riscv.cpp
@@ -28,7 +28,6 @@
 #include "asm/macroAssembler.inline.hpp"
 #include "gc/shared/barrierSetAssembler.hpp"
 #include "gc/shared/collectedHeap.hpp"
-#include "gc/shared/tlab_globals.hpp"
 #include "interpreter/interp_masm.hpp"
 #include "interpreter/interpreter.hpp"
 #include "interpreter/interpreterRuntime.hpp"
