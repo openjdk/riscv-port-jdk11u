a854546523bbc21c6c537649da08aa3db59e7c0f
Dingli Zhang <dingli@iscas.ac.cn>
2024-10-16 22:07:45 +0800

    8294366: RISC-V: Partially mark out incompressible regions

--- src/hotspot/cpu/riscv/c1_MacroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/c1_MacroAssembler_riscv.cpp
@@ -321,7 +321,8 @@ void C1_MacroAssembler::build_frame(int framesize, int bang_size_in_bytes) {
   // first instruction with a jump. For this action to be legal we
   // must ensure that this first instruction is a J, JAL or NOP.
   // Make it a NOP.
-  nop();
+  IncompressibleRegion ir(this);  // keep the nop as 4 bytes for patching.
+  nop();  // 4 bytes
 
   assert(bang_size_in_bytes >= framesize, "stack bang size incorrect");
   // Make sure there is enough stack space for this method's activation.
